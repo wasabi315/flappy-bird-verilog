780dd2d49edc54edf5c724f50eeeaeb983216923
wasabi315 <wasabi.nvim.st@gmail.com>
2020-11-19 06:02:19 +0900

    kbhit

--- input.c
+++ input.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
+#include <sys/ioctl.h>
+#include <stdlib.h>
+
+int _kbhit() {
+    static int initialized = 0;
+
+    if (!initialized) {
+        struct termios term;
+        tcgetattr(STDIN_FILENO, &term);
+        term.c_lflag &= ~ICANON;
+        tcsetattr(STDIN_FILENO, TCSANOW, &term);
+        setbuf(stdin, NULL);
+        initialized = 1;
+    }
+
+    int bytesWaiting;
+    ioctl(STDIN_FILENO, FIONREAD, &bytesWaiting);
+    return bytesWaiting;
+}
 
 double fps = 60.0;
 
@@ -14,9 +33,12 @@ int main(int argc, char **argv) {
     parse_env(argc, argv);
 
     while (1) {
-        printf("x");
+        usleep(1000000 / fps);
+        if (_kbhit()) {
+            putchar(getchar());
+        } else {
+            putchar(0);
+        }
         fflush(stdout);
-        sleep(1 / fps);
     }
 }
-
